3623e41937f7ab7e87228e48eb676ca9185eb0f9
linkinparkrules <nghiacao1997@gmail.com>
2022-11-15 19:08:18 +0700

    add skills

--- src/Pages/Home/Background2/index.js
+++ src/Pages/Home/Background2/index.js
@@ -1,15 +1,23 @@
 import bg2 from "./bg2.module.css"
+import Skills from "./Skills";
 
 const Background2 = () => {
   return (
-    <div className={bg2.background2}>
-      <div className={bg2.intro}>
+    <>
+      <div className={bg2.background2}>
+        <div className={bg2.intro}>
           As someone who is organized and good with logic, i want to be a part of the main workforce
-        for the company, to give optimal solutions for the customers. Also, i want to increase my capacity
-        and ability through the company's professional work environment.
+          for the company, to give optimal solutions for the customers. Also, i want to increase my capacity
+          and ability through the company's professional work environment.
+        </div>
       </div>
+      <div className={bg2.skillsBoard}>
+        <Skills />
+        <Skills />
+        <Skills />
+      </div>
+    </>
 
-    </div>
   );
 };
 
