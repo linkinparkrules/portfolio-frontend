b9ea165ae42c2ad205a99fc0229ed37ba644cd8c
linkinparkrules <nghiacao1997@gmail.com>
2022-11-21 14:05:37 +0700

    update responsive

--- src/Pages/Home/Background1/index.js
+++ src/Pages/Home/Background1/index.js
@@ -11,7 +11,7 @@ const Background1 = () => {
   return (
     <div className={bg1.background1}>
       <h1 className={bg1.jobIntro}>Fullstack Developer</h1>
-      <p>Hi, my name is Nghia. Nice to meet you.</p>
+      <p>Hi, my name is Nghia Cao. Nice to meet you.</p>
       <img className={bg1.avatar} src={avatar} alt="avatar"/>
       <img className={bg1.devices} src={devices} alt="devices"/>
     </div>
